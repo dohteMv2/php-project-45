18744f78f2f5db7a0b330eaa2d1e24033c297aa2
dohteMv2 <liga-2akk@mail.ru>
2024-06-17 11:18:34 +0300

    afasf

---
+++ src/Cli.php
@@ -0,0 +1,7 @@
+namespace BrainGames\Cli
+use function cli\line;
+use function cli\prompt;
+
+line('Welcome to the Brain Game!');
+$name = prompt('May I have your name?');
+line("Hello, %s!", $name);
