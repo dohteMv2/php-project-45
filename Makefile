d2f70913f1272726a689220bab4ca888a0b08b42
dohteMv2 <liga-2akk@mail.ru>
2024-10-02 12:39:16 +0300

    reserv

--- Makefile
+++ Makefile
@@ -9,3 +9,9 @@ lint:
 	composer exec --verbose phpcs -- --standard=PSR12 src bin
 brain-even:
 	./bin/brain-even
+brain-calc:
+	./bin/brain-calc
+brain-gcd:
+	./bin/brain-gcd
+brain-progression:
+	./bin/brain-progression
---
+++ src/Engine.php
@@ -0,0 +1,53 @@
+<?php
+namespace BrainGames\Engine;
+
+
+
+use function cli\line;
+use function cli\prompt;
+
+function engine($question1, $answer1, $question2, $answer2, $question3, $answer3, $test)
+{
+line('Welcome to the Brain Game!');
+$name = prompt('May I have your name?');
+line("Hello, %s!", $name);
+line($test);
+
+line("Question: %s!", $question1);
+$answer = prompt('Your answer: ');
+if ($answer == $answer1)
+   {
+      line("Correct!");
+   }
+else if ($answer != $answer1)
+   {
+      return line("%s! is wrong answer ;(. Correct answer was $answer1.
+Let's try again, %s!", $answer, $name);
+   }
+
+line("Question: %s!", $question2);
+$answer = prompt('Your answer: ');
+if ($answer == $answer2)
+   {
+      line("Correct!");
+   }
+else if ($answer != $answer2)
+   {
+      return line("%s! is wrong answer ;(. Correct answer was $answer2.
+Let's try again, %s!", $answer, $name);
+   }
+
+line("Question: %s!", $question3);
+$answer = prompt('Your answer: ');
+if ($answer == $answer3)
+   {
+      line("Correct!");
+   }
+else if ($answer != $answer3)
+   {
+      return line("%s! is wrong answer ;(. Correct answer was $answer3.
+Let's try again, %s!", $answer, $name);
+   }
+return line("Congratulations, %s!", $name);
+
+}
