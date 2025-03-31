a034ab1366d95df42531022be483d1ea9158fac3
dohteMv2 <liga-2akk@mail.ru>
2025-03-31 11:33:37 +0300

    correct

--- src/Engine.php
+++ src/Engine.php
@@ -5,7 +5,7 @@ namespace BrainGames\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function engine($question1, $answer1, $question2, $answer2, $question3, $answer3, $test)
+function engine(string $question1, string $answer1, string $question2, string $answer2, string $question3, string $answer3, string $test)
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
